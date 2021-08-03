d4a3f333606ef26cd7b2fd6323724cc3ac5abbe0
Joe Christensen <joechristensenwork@gmail.com>
2021-08-03 16:22:35 -0700

    Started adding post search functionality

--- src/components/Forums.js
+++ src/components/Forums.js
@@ -1,17 +1,81 @@
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Grid, Box, Container } from "@material-ui/core"
 import NavBar from "./NavBar";
+import SearchBar from "material-ui-search-bar";
 
-function Forums({user}) {
+function Forums({ user }) {
+    const [posts, setPosts] = useState([]);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [search, setSearch] = useState({
+        value: ""
+    })
     const history = useHistory();
-
-    if(!user) {
+    if (!user) {
         history.push("/");
     }
+
+    useEffect(() => {
+        fetch("http://localhost:8000/posts")
+            .then(res => res.json())
+            .then(data => setPosts(data))
+    }, [])
+
+    const filteredPosts = posts.filter((post) => {
+        if(search.value==="") return false;
+        if(post.title.toLowerCase().includes(search.value.toLowerCase())) return true;
+        return false;
+    })
 
     return (
         <div>
             <NavBar />
-
+            <SearchBar
+                style={{
+                    marginTop: "20px",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    width: "80%"
+                }}
+                value={search.value}
+                onCancelSearch={
+                    () => {
+                        setSearch({value: ""});
+                    }
+                }
+                onRequestSearch={
+                    (newValue) => {
+                        setSearch({ value: newValue })
+                    }
+                }
+            />
+            <Container>
+                <Box
+                    marginTop="40px"
+                    margin="20px"
+                    display="flex"
+                    alignItems="top"
+                    justifyContent="top"
+                    overflow="hidden"
+                    textAlign="center"
+                >
+                    <Grid container spacing={3}>
+                        {
+                            filteredPosts.map((post) => {
+                                return (
+                                    <Grid
+                                        item
+                                        xs={12}
+                                        key={post.id}
+                                    >
+                                        <h2>{post.title}</h2>
+                                    </Grid>
+                                );
+                            })
+                        }
+                    </Grid>
+                </Box>
+            </Container>
         </div>
     );
 }
